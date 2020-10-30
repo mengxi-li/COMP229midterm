191a07b0cb504473d054a5015b4a40f5a13ee896
mengxi li <mengxi616@gmail.com>
2020-10-29 22:03:26 -0400

    save book

--- server/routes/books.js
+++ server/routes/books.js
@@ -4,16 +4,17 @@ let router = express.Router();
 let mongoose = require('mongoose');
 
 // define the book model
-let book = require('../models/books');
+let Book = require('../models/books');
 
 /* GET books List page. READ */
 router.get('/', (req, res, next) => {
   // find all books in the books collection
-  book.find( (err, books) => {
+  Book.find( (err, books) => {
     if (err) {
       return console.error(err);
     }
     else {
+      console.log(books)
       res.render('books/index', {
         title: 'Books',
         books: books
@@ -25,19 +26,20 @@ router.get('/', (req, res, next) => {
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-  res.render('books/add', {
-    title: 'books', books: {
-      title: '',
-      description: '',
-      price: '',
-      author: '',
-      genre: ''
-    }
+  res.render('books/details', {
+    title: 'books' ,books: {
+      Title: '',
+      Description: '',
+      Price: '',
+      Author: '',
+      Genre: ''
+    }, 
+      
 });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
-router.post('/add', (req, res, next) => {
+router.post('/add',async (req, res, next) => {
   const book = await new Book(req.body);
   console.log(book);
   try {
@@ -50,7 +52,7 @@ router.post('/add', (req, res, next) => {
 });
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/:id', (req, res, next) => {
+router.get('/details/:id',async (req, res, next) => {
   const _id = req.params.id;
   try {
     const books = await Book.findById(_id);
@@ -59,20 +61,21 @@ router.get('/:id', (req, res, next) => {
     }
     res.render('books/details', { title: 'Edit Book', books });
   } catch (e) {
+    console.log(e)
     res.status(500).send();
   }
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/:id', (req, res, next) => {
+router.post('/details/:id',async (req, res, next) => {
   const _id = req.params.id;
   try {
     const book = await Book.findByIdAndUpdate(_id, {
-      title: req.body.title,
-      description: req.body.description,
-      price: req.body.price,
-      author: req.body.author,
-      genre: req.body.genre
+      Title: req.body.Title,
+      Description: req.body.Description,
+      Price: req.body.Price,
+      Author: req.body.Author,
+      Genre: req.body.Genre
     })
     res.redirect('/books');
   } catch (e) {
@@ -82,7 +85,7 @@ router.post('/:id', (req, res, next) => {
 });
 
 // GET - process the delete by user id
-router.get('/delete/:id', (req, res, next) => {
+router.get('/delete/:id', async(req, res, next) => {
   try {
     const book = await Book.findByIdAndDelete(req.params.id);
     res.redirect('/books')
